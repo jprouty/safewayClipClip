308ee41e1b6afc802c80afda7f51a458f978674e
Jeff Prouty <jeff.prouty@gmail.com>
2025-01-09 11:48:30 -0800

    Fix up clip clip to work again.

--- safewayclipclip/args.py
+++ safewayclipclip/args.py
@@ -1,6 +1,6 @@
 import os
 
-BASE_PATH = os.path.join(os.path.expanduser("~"), 'SafewayClipClip')
+BASE_PATH = os.path.join(os.path.expanduser("~"), "SafewayClipClip")
 
 
 def get_name_to_help_dict(parser):
@@ -11,28 +11,43 @@ def define_common_args(parser):
     """Parseargs shared between both CLI & GUI programs."""
     # Amazon creds:
     parser.add_argument(
-        '--safeway_username', default=None,
-        help=('Safeway username, either an e-mail or phone. If not provided, '
-              'you will be prompted for it.'))
+        "--safeway_username",
+        default=None,
+        help=(
+            "Safeway username, either an e-mail or phone. If not provided, "
+            "you will be prompted for it."
+        ),
+    )
     parser.add_argument(
-        '--safeway_user_will_login',
-        action='store_true',
+        "--safeway_password",
+        default=None,
+        help=("Safeway password. If not provided, " "you will be prompted for it."),
+    )
+    parser.add_argument(
+        "--safeway_user_will_login",
+        action="store_true",
         default=False,
-        help='If set, let the user log in on their own.')
+        help="If set, let the user log in on their own.",
+    )
 
-    default_session_path = os.path.join(BASE_PATH, 'ChromeSession')
+    default_session_path = os.path.join(BASE_PATH, "ChromeSession")
     parser.add_argument(
-        '--session-path', nargs='?',
+        "--session-path",
+        nargs="?",
         default=default_session_path,
-        help=('Directory to save browser session, including cookies. Use to '
-              'prevent repeated MFA prompts. Defaults to a directory in your '
-              'home dir. Set to None to use a temporary profile.'))
+        help=(
+            "Directory to save browser session, including cookies. Use to "
+            "prevent repeated MFA prompts. Defaults to a directory in your "
+            "home dir. Set to None to use a temporary profile."
+        ),
+    )
     parser.add_argument(
-        '--headless',
-        action='store_true',
+        "--headless",
+        action="store_true",
         default=False,
-        help='Whether to execute chromedriver with no visible window.')
+        help="Whether to execute chromedriver with no visible window.",
+    )
 
     parser.add_argument(
-        '-V', '--version', action='store_true',
-        help='Shows the app version and quits.')
+        "-V", "--version", action="store_true", help="Shows the app version and quits."
+    )
--- safewayclipclip/cli.py
+++ safewayclipclip/cli.py
@@ -7,25 +7,36 @@ import atexit
 from datetime import datetime
 import logging
 import os
+from pprint import pprint
 import random
 import time
 
 from selenium.common.exceptions import (
-    ElementClickInterceptedException, StaleElementReferenceException)
+    ElementClickInterceptedException,
+    JavascriptException,
+    StaleElementReferenceException,
+)
+from selenium.webdriver import ActionChains
 
 from safewayclipclip import VERSION
 from safewayclipclip.args import define_common_args, BASE_PATH
 from safewayclipclip.webdriver import (
     get_webdriver,
-    get_element_by_id, get_element_by_name, get_element_by_xpath,
-    get_element_by_link_text, get_elements_by_class_name, is_visible)
+    get_element_by_id,
+    get_element_by_name,
+    get_element_by_xpath,
+    get_element_by_link_text,
+    get_elements_by_class_name,
+    get_elements_by_xpath,
+    is_visible,
+)
 
 
 logger = logging.getLogger(__name__)
 
 
-SAFEWAY_HOME = 'https://www.safeway.com'
-FOR_U = '{}/justforu/coupons-deals.html'.format(SAFEWAY_HOME)
+SAFEWAY_HOME = "https://www.safeway.com"
+FOR_U = "{}/justforu/coupons-deals.html".format(SAFEWAY_HOME)
 
 
 def main():
@@ -38,58 +49,93 @@ def main():
     # For helping remote debugging, also log to file.
     # Developers should be vigilant to NOT log any PII, ever (including being
     # mindful of what exceptions might be thrown).
-    log_directory = os.path.join(BASE_PATH, 'Logs')
+    log_directory = os.path.join(BASE_PATH, "Logs")
     os.makedirs(log_directory, exist_ok=True)
-    log_filename = os.path.join(log_directory, '{}.log'.format(
-        time.strftime("%Y-%m-%d_%H-%M-%S")))
+    log_filename = os.path.join(
+        log_directory, "{}.log".format(time.strftime("%Y-%m-%d_%H-%M-%S"))
+    )
     root_logger.addHandler(logging.FileHandler(log_filename))
 
-    parser = argparse.ArgumentParser(description='Clip Safeway coupons.')
+    parser = argparse.ArgumentParser(description="Clip Safeway coupons.")
     define_common_args(parser)
     args = parser.parse_args()
 
     if args.version:
-        print('SafewayClipClip {}\nBy: Jeff Prouty'.format(VERSION))
+        print("SafewayClipClip {}\nBy: Jeff Prouty".format(VERSION))
         exit(0)
 
     webdriver = get_webdriver(args.headless, args.session_path)
-    webdriver.implicitly_wait(5)
+    webdriver.implicitly_wait(2)
 
     def close_webdriver():
         webdriver.close()
+
     atexit.register(close_webdriver)
 
-    login(webdriver, args)
     webdriver.get(FOR_U)
+    if not login_if_needed(webdriver, args):
+        logger.error("Cannot login - exiting")
+        time.sleep(60)
+        return
 
-    while True:
-        coupons_clip_clip = get_elements_by_class_name(
-            webdriver, 'grid-coupon-btn')
-        if not coupons_clip_clip:
-            logger.error('Cannot find coupons')
-        else:
-            for c in coupons_clip_clip:
-                try:
-                    user_click(c)
-                except ElementClickInterceptedException:
-                    logger.exception('Click interception error; continuing')
+    # Accept cookies bottom
 
-        load_mores = get_elements_by_class_name(webdriver, 'load-more')
-        if not load_mores or not is_visible(load_mores[0]):
-            logger.warning(
-                'Cannot find "Load more" button; either done or unexpectedly '
-                'missing')
-            break
-        logger.info('Clicking load more!')
-        user_click(load_mores[0])
-        time.sleep(2)
-    logger.info('All done! Sleeping for 5m before exiting to allow for review')
+    # while True:
+    coupons_clip_clip = get_elements_by_xpath(
+        webdriver, '//button[contains(text(), "Clip Coupon")]'
+    )
+    if not coupons_clip_clip:
+        logger.error("Cannot find coupons")
+        time.sleep(60)
+        return
+
+    accept_all_cookies = get_element_by_xpath(
+        webdriver, '//button[contains(text(), "Accept All")]'
+    )
+    if is_visible(accept_all_cookies):
+        user_click(webdriver, accept_all_cookies)
+
+    # Avoid stale ref error by getting a new list of buttons after each click
+    while coupons_clip_clip:
+        try:
+            user_click(webdriver, coupons_clip_clip[0])
+        except ElementClickInterceptedException:
+            logger.exception("Click interception error; continuing")
+        except StaleElementReferenceException:
+            logger.exception("Stale ref error; continuing")
+        except JavascriptException:
+            logger.exception("JS error; continuing")
+
+        time.sleep(1)
+
+        close_modal_button = get_element_by_xpath(
+            webdriver, '//*[@id="errorModal"]//button[contains(text(), "Close")]'
+        )
+        if is_visible(close_modal_button):
+            user_click(webdriver, close_modal_button)
+
+        coupons_clip_clip = get_elements_by_xpath(
+            webdriver, '//button[contains(text(), "Clip Coupon")]'
+        )
+
+        # No need to load more - clipped ones disappear and new ones come in.
+        # load_mores = get_elements_by_class_name(webdriver, "load-more")
+        # if not load_mores or not is_visible(load_mores[0]):
+        #     logger.warning(
+        #         'Cannot find "Load more" button; either done or unexpectedly ' "missing"
+        #     )
+        #     break
+        # logger.info("Clicking load more!")
+        # user_click(load_mores[0])
+        # time.sleep(2)
+    logger.info("All done! Sleeping for 5m before exiting to allow for review")
     time.sleep(60 * 5)
 
 
-def user_click(elem):
+def user_click(webdriver, elem):
     rand_user_delay()
-    elem.click()
+    ActionChains(webdriver).move_to_element(elem).click().perform()
+    # elem.click()
 
 
 def rand_user_delay():
@@ -102,99 +148,53 @@ def on_critical(msg):
     exit(1)
 
 
-def login(webdriver, args):
+def login_if_needed(webdriver, args):
+    # Already logged in.
+    if get_elements_by_class_name(webdriver, "menu-nav__profile-button"):
+        return True
+
     maybe_prompt_for_safeway_credentials(args)
-    logger.info('Navigating to Safeway homepage.')
-    webdriver.get(SAFEWAY_HOME)
 
-    menu_buttons = get_elements_by_class_name(
-        webdriver, 'menu-nav__profile-button')
-    if not menu_buttons:
-        logger.error('Cannot find menu element')
+    username_input = get_element_by_id(webdriver, "enterUsername")
+    if not username_input:
+        logger.error("Cannot find user input element")
         return False
-    menu_button = menu_buttons[0]
-    from pprint import pprint
-    pprint(menu_button)
-    if menu_button.text.strip().lower() == 'account':
-        logger.info('Login menu button indicates user is already logged in')
-        return True
-    user_click(menu_button)
+    rand_user_delay()
+    username_input.send_keys(args.safeway_username)
 
-    login_button = get_element_by_id(webdriver, 'sign-in-modal-link')
-    if not login_button:
-        logger.error('Cannot find login menu item')
+    sign_in_with_password_button = get_element_by_xpath(
+        webdriver, '//button[contains(text(), "Sign in with password")]'
+    )
+    if not sign_in_with_password_button:
+        logger.error("Cannot find 'Sign in with password' button")
         return False
-    if login_button.text.strip().lower() != 'sign in':
-        logger.info('Login menu indicates user is already logged in')
-        return True
-    user_click(login_button)
+    user_click(webdriver, sign_in_with_password_button)
 
-    login_via_otps = get_elements_by_class_name(webdriver, 'otp-link')
-    if not login_via_otps:
-        logger.error('Cannot find login via OTP link')
+    password_input = get_element_by_id(webdriver, "password")
+    if not password_input:
+        logger.error("Cannot find password input element")
         return False
-    login_via_otp = login_via_otps[0]
-    user_click(login_via_otp)
+    rand_user_delay()
+    password_input.send_keys(args.safeway_password)
 
-    user_input = get_element_by_id(webdriver, 'email-mobile-otp')
-    if not user_input:
-        logger.error('Cannot find username input')
+    sign_in_button = get_element_by_xpath(
+        webdriver, '//button[contains(text(), "Sign In")]'
+    )
+    if not sign_in_button:
+        logger.error("Cannot find 'Sign In' button")
         return False
-    user_input.send_keys(args.safeway_username)
+    user_click(webdriver, sign_in_button)
 
-    otp_submit = get_element_by_id(webdriver, 'update-otp-continue-btn')
-    if not otp_submit:
-        logger.error('Cannot find OTP submit button')
-        return False
-    user_click(otp_submit)
-
-    otp_code = get_element_by_id(webdriver, 'otp-code')
-    if not otp_code:
-        logger.error('Cannot find OTP code input')
-        return False
-
-    otp_code_value = input('Enter the OTP code you received at {}: '.format(
-        args.safeway_username))
-    otp_code.send_keys(otp_code_value)
-    time.sleep(1)
-
-    otp_submit = get_element_by_id(webdriver, 'sign-in-otp-btn')
-    if not otp_submit:
-        logger.error('Cannot find OTP submit button')
-        return False
-    user_click(otp_submit)
-
-    return wait_for_login(webdriver)
-
-
-def wait_for_login(webdriver, max_wait_s=1 * 60):
-    start_time = datetime.now()
-    while True:
-        time.sleep(1)
-        since_start = datetime.now() - start_time
-        if since_start.total_seconds() > max_wait_s:
-            logger.error('Exceeded timeout waiting for authed homepage')
-            return False
-        try:
-            menu_buttons = get_elements_by_class_name(
-                webdriver, 'menu-nav__profile-button-sign-in-up')
-            if not menu_buttons:
-                continue
-            menu_button = menu_buttons[0]
-            if menu_button.text.strip().lower() == 'account':
-                break
-            user_click(menu_button)
-        except StaleElementReferenceException:
-            logger.warning(
-                'Stale reference while waiting for homepage to load')
-    logger.info('Authed login homepage - login complete')
-    return True
+    logger.info("Login flow complete!")
+    return get_elements_by_class_name(webdriver, "menu-nav__profile-button")
 
 
 def maybe_prompt_for_safeway_credentials(args):
     if not args.safeway_username and not args.safeway_user_will_login:
-        args.safeway_username = input('Safeway email or phone: ')
+        args.safeway_username = input("Safeway email or phone: ")
+    if not args.safeway_password and not args.safeway_user_will_login:
+        args.safeway_password = input("Safeway password: ")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
